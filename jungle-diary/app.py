eaa2af05b619fede6631af265e9edb29cad3ede4
gcount85 <100mgml@gmail.com>
2022-10-25 01:08:44 +0900

    기본 템플릿 login, sign_up 추가

--- jungle-diary/app.py
+++ jungle-diary/app.py
@@ -1,7 +1,7 @@
-import requests
-from flask import Flask, request, render_template, jsonify, make_response 
+from flask import Flask, request, render_template, jsonify, make_response, redirect, url_for
 from pymongo import MongoClient 
-from bs4 import BeautifulSoup
+# 왜인지 hashlib 설치가 되지 않음 
+import hashlib
 import jwt
 from datetime import datetime, timedelta
 
@@ -9,36 +9,54 @@ app = Flask(__name__)
 # app.config["TEMPLATES_AUTO_RELOAD"] = True
 # app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
 
-admin_id = "Minsu"
-admin_pw = "123456"
-SECRET_KEY = 'apple'
+SECRET_KEY = 'SPARTA'
 
 # client = MongoClient('mongodb://13.124.117.232', 27017, username="test", password="test")  # aws연결
 client = MongoClient('localhost', 27017)  # 로컬연결
 db = client.db_jungle_local
 
 
+@app.route('/')
+def home():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+
+        return render_template('index.html')
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
 
-@app.route("/login", methods=['POST'])
-def login_proc():
-	print(request.form)
-	input_data = request.form
-	user_id = input_data['id']
-	user_pw = input_data['pw']
-
-	# 아이디, 비밀번호가 일치하는 경우
-	if (user_id == admin_id and
-			user_pw == admin_pw):
-		payload = {
-			'id': user_id,
-			'exp': datetime.utcnow() + timedelta(seconds=60)  # 로그인 24시간 유지
-		}
-		token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
-
-		return jsonify({'result': 'success', 'token': token})
+@app.route('/login')
+def login():
+    msg = request.args.get("msg")
+    return render_template('login.html', msg=msg)
 
 
-	# 아이디, 비밀번호가 일치하지 않는 경우
-	else:
-		return jsonify({'result': 'fail'})
+@app.route('/sign_in', methods=['POST'])
+def sign_in():
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+
+    if result is not None:  #result 찾음
+        payload = {
+            'id': username_receive,
+            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
+
+        return jsonify({'result': 'success', 'token': token})
+    # result 못찾음
+    else:
+        return jsonify({'result': 'fail', 'msg': '가입하지 않은 아이디이거나, 잘못된 비밀번호입니다.'})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
+
+
